88c9cffa7bd719ad0d0479ae5dfadab0ee87bf15
Busna27 <lucabusnelli2001@gmail.com>
2022-03-03 16:48:27 +0100

    i discover that my computer does not kwon how to do sum

--- js/script.js
+++ js/script.js
@@ -13,6 +13,41 @@ function checkPalindrome(string) {
     return true;
 }
 
+/**
+ * This function take the value and tranform into parseInt
+ * 
+ * @param {*} numerone Insert the number
+ * @returns Return the parseInt of the value
+ */
+function parseIntInput(numerone) {
+    return parseInt(numerone.value)
+}
+
+/**
+ * This function generate a random number between 1 and 5
+ * 
+ * @returns A random int from 1 to 5
+ */
+function randomInteger() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+/**
+ * This function do a sum and check if the result is odd or even
+ * 
+ * @param {*} firstNum First number
+ * @param {*} secondNum Second Number
+ * @returns True(odd) or False(even)
+ */
+function oddSumNumbers(firstNum, secondNum) {
+    if((firstNum + secondNum)%2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 // EventListerner for the send btn
 const btnSend = document.getElementById('btn-pali');
 btnSend.addEventListener('click', function() {
@@ -34,4 +69,26 @@ btnSend.addEventListener('click', function() {
 
 
 
+const btnOddOrEven = document.getElementById('btn-odd-even');
+btnOddOrEven.addEventListener('click', function() {
+    let inputNumUser = document.getElementById('input-number');
+    if((parseIntInput(inputNumUser) > 5 || parseIntInput(inputNumUser) < 0) || (isNaN(inputNumUser.value)) ) {
+        inputNumUser.value = "";
+        location.reload();
+    }
+
+    console.log(`Input user: ${parseIntInput(inputNumUser)}`);
+    console.log(`Random computer: ${randomInteger()}`);
+    console.log(`Sum: ${(parseIntInput(inputNumUser)) + (randomInteger())}`);
+    console.log(`Result: ${oddSumNumbers(parseIntInput(inputNumUser), randomInteger())}`);
+
+    const oddP = document.getElementById('result-odd-p');
+    if(oddSumNumbers(parseIntInput(inputNumUser), randomInteger()) == true) {
+        oddP.innerHTML = "E' pari!!!"
+    } else {
+        oddP.innerHTML = "E' dispari!!!"
+    }
+
+    inputNumUser.value = "";
+});
 
